f08fe10266a11daa394e51b6a2b9c846793e3b1b
Mosharraf Musa <mosharrafny@gmail.com>
2020-05-14 18:59:43 -0400

    Add Movies Working - STRETCH GOAL

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
+import AddMovie from "./Movies/AddMovie";
 import Movie from "./Movies/Movie";
 import axios from "axios";
 
@@ -29,6 +30,7 @@ const App = () => {
       <SavedList list={savedList} />
 
       <Route exact path="/">
+        <AddMovie getMovieList={getMovieList} />
         <MovieList movies={movieList} />
       </Route>
 
