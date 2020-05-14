056e7d1fd58214f19c7ec1b10b61bfe33847ad2f
Mosharraf Musa <mosharrafny@gmail.com>
2020-05-14 18:30:57 -0400

    Updating Movie Title

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,16 +1,32 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
-function Movie({ addToSavedList }) {
+function Movie({ addToSavedList, getMovieList }) {
   const [movie, setMovie] = useState(null);
+  const [updates, setUpdates] = useState("");
   const params = useParams();
+  const history = useHistory();
 
   const fetchMovie = (id) => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then((res) => setMovie(res.data))
+      .catch((err) => console.log(err.response));
+  };
+
+  const updateMovie = (e) => {
+    e.preventDefault();
+    axios
+      .put(`http://localhost:5000/api/movies/${params.id}`, {
+        ...movie,
+        title: updates,
+      })
+      .then((res) => {
+        console.log(res);
+        setMovie(res.data);
+      })
       .catch((err) => console.log(err.response));
   };
 
@@ -33,6 +49,15 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+      <form onSubmit={updateMovie}>
+        <h2>Update movie title</h2>
+        <input
+          type="text"
+          onChange={(e) => setUpdates(e.target.value)}
+          value={updates}
+        />
+        <button>Submit updates</button>
+      </form>
     </div>
   );
 }
